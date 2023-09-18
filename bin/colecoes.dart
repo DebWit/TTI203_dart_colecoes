999383ac26906cd4d537346007d9a117d88f1425
DebWit <deborawit@gmail.com>
2023-09-18 07:15:08 -0300

    Update colecoes.dart

--- bin/colecoes.dart
+++ bin/colecoes.dart
@@ -1,19 +1,109 @@
 import 'dart:math';
-import 'dart:io';
-void exercicio2(){
-  List <int> listaEntradas = [];
-  while (listaEntradas.length<6) {
-    print("Digite um número: ");
-    var entrada = stdin.readLineSync()!;
-    var numero = int.parse(entrada);
-    if(!listaEntradas.contains(numero)){
-      listaEntradas.add(numero);
-    }
-    else {
-      print("Número Duplicado");
-  }}
 
-  print(listaEntradas);
+
+void exercicio1(){
+  var portugues = {'Brasil', 'Portugal'};
+  var europa = {'Alemanha', 'Portugal', 'Espanha'};
+  //a) todos os paises em que se fala portugues e todos os paises da europa
+  print(portugues.union(europa));
+  //b) todos os paises em que se fala portugues e que são europeus
+    print(portugues.intersection(europa));
+  //c) fala portugues mas não sao europeus
+  print(portugues.difference(europa));
+  //d) não pode falar portugues nem ser da europa
+  print(europa.difference(portugues));
+}
+
+
+
+
+
+void main(){
+  exercicio1();
+
+  // //lista com final
+
+  // final nomes = ['Ana', 'Pedro'];
+  // nomes[0] = 'Ana Maria';
+  // nomes.add('João');
+  // // isso não vale
+  // // nomes = [];
+
+  // _____________________________ lista com const
+  // const nomes = ['Ana', 'Pedro'];
+   // // isso não vale
+  // // nomes = [];
+  // nomes[0] = 'Ana Maria';
+  // nomes.add('João');
+
+  // da erro de execução
+
+  // ________________________ sound null safety (não tem perigo de ser null a nao ser que voce peça diretamente)
+  // var nomes = ['Ana', 'Pedro'];
+  // não deixa 
+  //nomes[0] = null, 
+  //nem 
+  //nomes = null, 
+  //nem 
+  //nomes.add(null)
+  
+  // var nomes2 = [];
+  // print(nomes2.runtimeType); // List<dynamic>
+
+  //List <String> nomes3 = //<= não pode ser iguakl a nul nem contar um item/objeto da lista como null
+
+
+  // List <String?> nomes4 = []; // a interrogação faz com que não seja obrigatorio que seja string
+  // para poder tornar a lista = a null e os objetos poderem ser null: List <String?>? nomes = ...
+
+  // _______________________________Lista de listas ________________________________________________
+  // List < List <int?> >? >? nome = ...
+
+  // Records ou tuplas: lista imutável
+  // var tupla = ('Ana', 18, true);
+  // print(tupla.runtimeType); // (String, int, bool)
+
+  // tupla.$1 (referencia um item da tupla)
+
+  // print(tupla[0]); não funciona porque a contagem começa do 1
+
+  //__________________________________ CONJUNTOS: notação é {} _____________________________________
+  // var nomes = {'João', 'Ana'};
+  // nomes.runtimeType =>_Set<String>
+  // var teste = {}; (é um _Map)
+  // conjuntos não tem ordem e nem itens repetidos, se voce criar um com itens repetidos, ele não dara 
+  //erro mas removerá as duplicatas
+
+// nomes = {'Ana', 'João'}
+  // referenciar um elemento: 
+  // nomes.elementAt(0)
+
+
+
+}
+
+
+
+
+
+
+
+// import 'dart:math';
+// import 'dart:io';
+// void exercicio2(){
+//   List <int> listaEntradas = [];
+//   while (listaEntradas.length<6) {
+//     print("Digite um número: ");
+//     var entrada = stdin.readLineSync()!;
+//     var numero = int.parse(entrada);
+//     if(!listaEntradas.contains(numero)){
+//       listaEntradas.add(numero);
+//     }
+//     else {
+//       print("Número Duplicado");
+//   }}
+
+//   print(listaEntradas);
   
   /*
   Exercício. Escreva um programa que:
@@ -24,7 +114,7 @@ void exercicio2(){
   - mostra ao usuário quais números ele acertou.
   */
 
-}
+//}
 
 // void exercicio1(List <String> arguments){/*
 //     Exercício. Escreva um programa que faz a soma dos elementos recebidos como
@@ -40,8 +130,8 @@ void exercicio2(){
 //   }
 //   print (contador);
 // }
-void main(List <String> arguments){
-  exercicio2();
+// void main(List <String> arguments){
+//   exercicio2();
   //type annotation
   // var somenteStrings = <String> [];
   // print(somenteStrings);
@@ -101,4 +191,4 @@ void main(List <String> arguments){
   // print(nomes.runtimeType);
   // print(nomes);
   //print(arguments);
-}
+//}
